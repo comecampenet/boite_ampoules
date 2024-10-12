e5f77eeccb540b7cb3d55e31160bf419d9f16336
comecampenet <come.campenet@orange.fr>
2024-10-12 22:51:09 +0200

    on/off lights without display functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,57 @@
 #include <Arduino.h>
 
 // ##### pins ####
+
 // input
 const uint8_t IN_A = 2;
 const uint8_t IN_B = 3;
 const uint8_t IN_C = 4;
 const uint8_t IN_D = 5;
+const uint8_t IN_CLOSED;
+
 // output
 
+
 // #### declarations ####
+
 // functions
-void display_number(short nb);
+void display_number(uint8_t nb);
+void clear_display();
 uint8_t get_nb_choice();
+
 // variables
-uint8_t nb_choice = 0; 
+uint8_t nb_choice = 0;
 
 // #### main code ####
+
 // main 
 void setup() {
   pinMode(IN_A,INPUT);
   pinMode(IN_B,INPUT);
   pinMode(IN_C,INPUT);
   pinMode(IN_D,INPUT);
+  pinMode(IN_CLOSED,INPUT);
   nb_choice = get_nb_choice();
   display_number(nb_choice);
 }
 
 void loop() {
-  u_int8_t new_nb_choice = get_nb_choice();
-  if (new_nb_choice != nb_choice)
-  {
-    nb_choice = new_nb_choice;
+  u_int8_t nb_choice = get_nb_choice();
+  if (digitalRead(IN_CLOSED))
     display_number(nb_choice);
-  }
+  else
+    clear_display();
 }
 
 // functions
 void display_number(short nb)
 {
 
+}
+
+void clear_display()
+{
+  
 }
 
 uint8_t get_nb_choice()
