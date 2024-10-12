a2d4d1c966d810f2384ffa6ede91908d3544cb58
comecampenet <come.campenet@orange.fr>
2024-10-12 19:44:07 +0200

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+
+void display_number(short nb);
+int get_nb_choice();
+
+void setup() {
+  display_number(get_nb_choice());
+}
+
+void loop() {
+  
+}
+
+void display_number(short nb)
+{
+
+}
