7dbed626894a83e345436a16230e90aba90d6806
comecampenet <come.campenet@orange.fr>
2024-10-15 11:15:57 +0200

    better format

--- src/main.cpp
+++ src/main.cpp
@@ -26,21 +26,28 @@ uint8_t nb_choice = 0;
 
 // main 
 void setup() {
+
   pinMode(IN_A,INPUT);
   pinMode(IN_B,INPUT);
   pinMode(IN_C,INPUT);
   pinMode(IN_D,INPUT);
   pinMode(IN_CLOSED,INPUT);
+  
   nb_choice = get_nb_choice();
+  
   display_number(nb_choice);
 }
 
 void loop() {
+  
   u_int8_t nb_choice = get_nb_choice();
+  
   if (digitalRead(IN_CLOSED))
     display_number(nb_choice);
+  
   else
     clear_display();
+
 }
 
 // functions
@@ -56,6 +63,7 @@ void clear_display()
 
 uint8_t get_nb_choice()
 {
+
   bool a = digitalRead(IN_A);
   bool b = digitalRead(IN_B);
   bool c = digitalRead(IN_C);
@@ -63,22 +71,32 @@ uint8_t get_nb_choice()
 
   if (!a && !b && !c && !d) 
     return 0;
+
   else if (!a && !b && !c && d) 
     return 1;
+
   else if (!a && !b && c && !d)
     return 2;
+
   else if (!a && !b && c && d)
     return 3;
+
   else if (!a && b && !c && !d)
     return 4;
+
   else if (!a && b && !c && d)
     return 5;
+
   else if (!a &&  b && c && !d)
     return 6;
+
   else  if (!a && b && c && d)
     return 7;
+
   else  if (a && !b && !c && !d)
     return 8;
-  else   if (a && !b && !c && d)
+
+  else  if (a && !b && !c && d)
     return 9;
+
 }
